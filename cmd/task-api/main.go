bfac692fc13dacc1c99653bfb9b6308980a23bdc
okb97 <hercule0903@icloud.com>
2025-07-27 17:35:37 +0900

    create GetTaskAll repository

--- cmd/task-api/main.go
+++ cmd/task-api/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/okb97/go-log-platform/db"
+import (
+	"fmt"
+
+	"github.com/okb97/go-log-platform/db"
+	"github.com/okb97/go-log-platform/internal/repository"
+)
 
 func main() {
 	db.InitDB()
+	repository.SeedTestData()
+	tasks, err := repository.GetAllTasks()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, task := range tasks {
+		fmt.Printf("%+v\n", task)
+	}
 }
