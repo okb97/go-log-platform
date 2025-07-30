1fd430a4cb5a9bd28f3e3c2012ae8c6689bdacab
okb97 <hercule0903@icloud.com>
2025-07-30 22:15:27 +0900

    create main.go

--- cmd/task-api/main.go
+++ cmd/task-api/main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/okb97/go-log-platform/db"
 	"github.com/okb97/go-log-platform/internal/repository"
+	"github.com/okb97/go-log-platform/internal/router"
 )
 
 func main() {
 	db.InitDB()
 	repository.SeedTestData()
-	tasks, err := repository.GetAllTasks()
-	if err != nil {
-		panic(err)
-	}
 
-	for _, task := range tasks {
-		fmt.Printf("%+v\n", task)
-	}
+	r := router.TaskRouter()
+
+	r.Run(":8080")
 }
