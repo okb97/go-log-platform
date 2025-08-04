05a3d99b22836ce2e1d79e2e8e1eaaa0fee00141
okb97 <hercule0903@icloud.com>
2025-08-04 17:45:40 +0900

    create UpdateTask service

--- internal/service/task_service.go
+++ internal/service/task_service.go
@@ -16,3 +16,7 @@ func CreateTask(task *model.Task) error {
 func DeleteTask(id uint) error {
 	return repository.DeleteTask(id)
 }
+
+func UpdateTask(task *model.Task) error {
+	return repository.UpdateTask(task)
+}
